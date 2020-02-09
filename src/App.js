31e6cc58287b96a945bc641a437abfce739da5e2
tshifhiwa <desmond_madima@yahoo.com>
2020-02-09 15:34:54 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import FirstComponent from './components/learning-example/FirstComponent'
 import logo from './logo.svg';
 import './App.css';
 
+
+// This is my skype id ==> live:.cid.73febdac7a6c327d
 
 
 function FirstApp(){
   return(
     <div className="App">
-      My firstComponet
+      FirstApp
     </div>
   );
 }
@@ -16,20 +19,53 @@ function FirstApp(){
 function App() {
   return (
     <div className="FirstComponent">
-      My hello App
+      My hello 
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FirstApp></FirstApp>
     </div>
   );
 }
 
 //class component
-class FirstComponent extends Component{
+
+class SecondComponent extends Component{
   render(){
     return (
-      <div className = "firstComponent">
-        firstComponent
+      <div className = "secondComponent">
+           <LearningComponents></LearningComponents>
       </div>
     );
   }
 }
+
+function ThirdComponent(){
+  return(
+    <div className = "ThirdComponent">
+    ThirdComponent
+  </div>
+);
+}
+
+class LearningComponents extends Component{
+  render(){
+    return (
+      <div className = "LearningComponents">
+        My hello world
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+        <FirstApp></FirstApp>
+      </div>
+    );
+  }
+}
+    
+
+ 
+
+
+
 
 export default App;
